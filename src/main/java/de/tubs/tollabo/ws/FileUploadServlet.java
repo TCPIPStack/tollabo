4061dad5591547d74877fd6709c3d7e091d0981c
TeKilla86 <Tobias.Schroer@gmail.com>
2015-02-04 17:45:36 +0100

    Upload von PDF Dateien nun möglich

--- src/main/java/de/tubs/tollabo/ws/FileUploadServlet.java
+++ src/main/java/de/tubs/tollabo/ws/FileUploadServlet.java
@@ -7,6 +7,8 @@
  */
 package de.tubs.tollabo.ws;
 
+import java.awt.Rectangle;
+import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
@@ -14,6 +16,9 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
 import java.io.PrintWriter;
+import java.io.RandomAccessFile;
+import java.nio.ByteBuffer;
+import java.nio.channels.FileChannel;
 import java.util.ArrayList;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -26,6 +31,8 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.Part;
 import net.coobird.thumbnailator.Thumbnails;
+import com.sun.pdfview.PDFFile;
+import com.sun.pdfview.PDFPage;
 
 /**
  * File upload servlet example
@@ -36,6 +43,7 @@ public class FileUploadServlet extends HttpServlet {
 
     private final String UPLOADS_PATH = "/home/glassfish4/glassfish/domains/domain1/applications/tollabo/uploads";
     private final String THUMBNAILS_PATH = "/home/glassfish4/glassfish/domains/domain1/applications/tollabo/uploads/thumbnails";
+    private final String PDF_PATH = "/home/glassfish4/glassfish/domains/domain1/applications/tollabo/pdf";
 
     private final static Logger LOGGER
             = Logger.getLogger(FileUploadServlet.class.getCanonicalName());
@@ -122,36 +130,57 @@ public class FileUploadServlet extends HttpServlet {
         final PrintWriter writer = response.getWriter();
 
         String testExtension = fileName.substring(fileName.length() - 3, fileName.length());
-        if (!testExtension.contains("jpg") && !testExtension.contains("png")) {
+        if (!testExtension.contains("jpg") && !testExtension.contains("png") && !testExtension.contains("pdf")) {
             writer.println("Please upload an image file of type *.jpg or *.png <br>");
-            writer.println("Your filetype: "+  testExtension + "<br>");
+            writer.println("Your filetype: " + testExtension + "<br>");
             writer.println("<meta http-equiv=\"refresh\" content=\"5;URL=/tollabo/#gallery\" />");
             writer.println("You are beeing redirect to the main page in five seconds.<br>");
             writer.println("Otherwise, click <a href=\"/tollabo/#gallery\" </a> here <br>");
         } else {
 
             try {
-                out = new FileOutputStream(new File(UPLOADS_PATH + File.separator
-                        + fileName));
-                filecontent = filePart.getInputStream();
+                if (testExtension.contains("pdf")) {
+                    out = new FileOutputStream(new File(PDF_PATH + File.separator + fileName));
+                    filecontent = filePart.getInputStream();
 
-                int read;
-                final byte[] bytes = new byte[1024];
+                    int read;
+                    final byte[] bytes = new byte[1024];
 
-                while ((read = filecontent.read(bytes)) != -1) {
-                    out.write(bytes, 0, read);
+                    while ((read = filecontent.read(bytes)) != -1) {
+                        out.write(bytes, 0, read);
+                    }
+                    
+                    PDF2Image(PDF_PATH + File.separator + fileName);
+                    
+                    writer.println("New file " + fileName + " created <br>");
+                    LOGGER.log(Level.INFO, "File {0} being uploaded to {1}",
+                            new Object[]{fileName, UPLOADS_PATH});
+                    writer.println("<meta http-equiv=\"refresh\" content=\"5;URL=/tollabo/#gallery\" />");
+                    writer.println("You are beeing redirect to the main page in five seconds.<br>");
+                    writer.println("Otherwise, click <a href=\"/tollabo/#gallery\" </a> here <br>");
+                    
+                } else {
+                    out = new FileOutputStream(new File(UPLOADS_PATH + File.separator + fileName));
+                    filecontent = filePart.getInputStream();
+
+                    int read;
+                    final byte[] bytes = new byte[1024];
+
+                    while ((read = filecontent.read(bytes)) != -1) {
+                        out.write(bytes, 0, read);
+                    }
+
+                    Thumbnails.of(new File(UPLOADS_PATH + File.separator + fileName))
+                            .size(160, 160)
+                            .toFile(new File(THUMBNAILS_PATH + File.separator + fileName));
+
+                    writer.println("New file " + fileName + " created <br>");
+                    LOGGER.log(Level.INFO, "File {0} being uploaded to {1}",
+                            new Object[]{fileName, UPLOADS_PATH});
+                    writer.println("<meta http-equiv=\"refresh\" content=\"5;URL=/tollabo/#gallery\" />");
+                    writer.println("You are beeing redirect to the main page in five seconds.<br>");
+                    writer.println("Otherwise, click <a href=\"/tollabo/#gallery\" </a> here <br>");
                 }
-
-                Thumbnails.of(new File(UPLOADS_PATH + File.separator + fileName))
-                        .size(160, 160)
-                        .toFile(new File(THUMBNAILS_PATH + File.separator + fileName));
-
-                writer.println("New file " + fileName + " created <br>");
-                LOGGER.log(Level.INFO, "File {0} being uploaded to {1}",
-                        new Object[]{fileName, UPLOADS_PATH});
-                writer.println("<meta http-equiv=\"refresh\" content=\"5;URL=/tollabo/#gallery\" />");
-                writer.println("You are beeing redirect to the main page in five seconds.<br>");
-                writer.println("Otherwise, click <a href=\"/tollabo/#gallery\" </a> here <br>");
 
             } catch (FileNotFoundException fne) {
                 writer.println("You either did not specify a file to upload or are "
@@ -183,5 +212,61 @@ public class FileUploadServlet extends HttpServlet {
     @Override
     public String getServletInfo() {
         return "Servlet that uploads files to a user-defined destination";
+    }
+
+    private void PDF2Image(String fileName) {
+        File file = new File(fileName);
+        RandomAccessFile raf;
+        try {
+            raf = new RandomAccessFile(file, "r");
+
+            FileChannel channel = raf.getChannel();
+            ByteBuffer buf = channel.map(FileChannel.MapMode.READ_ONLY, 0, channel.size());
+            PDFFile pdffile = new PDFFile(buf);
+            // draw the first page to an image
+            int num = pdffile.getNumPages();
+            PDFPage page = pdffile.getPage(0);
+
+            //get the width and height for the doc at the default zoom				
+            int width = (int) page.getBBox().getWidth();
+            int height = (int) page.getBBox().getHeight();
+
+            Rectangle rect = new Rectangle(0, 0, width, height);
+            int rotation = page.getRotation();
+            Rectangle rect1 = rect;
+            if (rotation == 90 || rotation == 270) {
+                rect1 = new Rectangle(0, 0, rect.height, rect.width);
+            }
+
+            //generate the image
+            BufferedImage img = (BufferedImage) page.getImage(
+                    rect.width, rect.height, //width & height
+                    rect1, // clip rect
+                    null, // null for the ImageObserver
+                    true, // fill background with white
+                    true // block until drawing is done
+            );
+
+            //Dateipfad vor fileName entfernen
+            String[] split = fileName.split("/");
+            fileName = split[split.length - 1];
+
+            //Dateiendung (pdf) von fileName entfernen
+            split = fileName.split(".");
+            String finalName = "";
+            for (int i = 0; i < split.length - 1; i++) {
+                finalName += split[i];
+            }
+            ImageIO.write(img, "png", new File(UPLOADS_PATH + File.separator + finalName + ".png"));
+
+            Thumbnails.of(new File(UPLOADS_PATH + File.separator + finalName))
+                    .size(160, 160)
+                    .toFile(new File(THUMBNAILS_PATH + File.separator + finalName));
+
+        } catch (FileNotFoundException e1) {
+            System.err.println(e1.getLocalizedMessage());
+        } catch (IOException e) {
+            System.err.println(e.getLocalizedMessage());
+        }
     }
 }
